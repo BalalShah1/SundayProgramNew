6ec887b10a522368233f5d1b83ffaf2175a4468b
Balal Shah <balal_shah94@hotmail.co.uk>
2023-11-20 19:06:46 +0000

    Add files via upload

---
+++ ConsoleApp1Tests1/ProgramTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void TestCountFirstDaySundays()
+        {
+            // Arrange
+            int startYear = 1901;
+            int endYear = 2000;
+
+            // Act
+            int result = Program.CountFirstDaySundays(startYear, endYear);
+
+            // Assert
+            Assert.AreEqual(171, result); 
+        }
+
+        [TestMethod]
+        public void TestCalculateDaysInMonth()
+        {
+            // Arrange
+            int nonLeapYear = 2023; // A non-leap year
+            int leapYear = 2024;    // A leap year
+
+            // Act and Assert 
+            Assert.AreEqual(28, Program.CalculateDaysInMonth(2, nonLeapYear)); // February (non-leap year)
+
+            // Assert 
+            Assert.AreEqual(29, Program.CalculateDaysInMonth(2, leapYear));
+        }
+    }
+}
